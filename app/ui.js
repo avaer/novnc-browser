5243f020a0efdc154f168efd6df2af280c5e0679
Adrian Biedrzycki <a@modules.io>
2017-05-15 04:54:45 -0700

    Clean up module packaging

--- app/ui.js
+++ app/ui.js
@@ -10,48 +10,23 @@ import * as WebUtil from "./webutil.js";
 
 import {Keyboard} from "../core/input/devices.js";
 
-const _connect = ({canvas, host, port, path, ondisconnect}) => {
+const _connect = ({
+  canvas = document.createElement('canvas'),
+  host = '127.0.0.1',
+  port = 8000,
+  path = '',
+  secure = false,
+  ondisconnect = null,
+} = {}) => {
   const rfb = new RFB({
     target: canvas,
-    // 'onNotification': UI.notification,
-    /* 'onUpdateState': (rfb, state, oldstate) => {
-      console.log('update state', JSON.stringify(state));
-    }, */
     onDisconnected: () => {
       ondisconnect && ondisconnect();
     },
-    // 'onPasswordRequired': UI.passwordRequired,
-    // 'onXvpInit': UI.updateXvpButton,
-    /* 'onClipboard': (rfb, text) => {
-      console.log('clipboard receive', JSON.stringify(text));
-    }, */
-    // 'onBell': UI.bell,
-    // 'onFBUComplete': UI.initialResize,
-    // 'onFBResize': UI.updateViewDrag,
-    /* 'onDesktopName': (rfb, name) => {
-      console.log('desktop name', JSON.stringify(name));
-    }, */
   });
-
-  /* const _sendClipboard = text => {
-    rfb.clipboardPasteFrom(text);
-  }; */
-
-  /* rfb.set_encrypt(UI.getSetting('encrypt'));
-  rfb.set_true_color(UI.getSetting('true_color'));
-  rfb.set_local_cursor(UI.getSetting('cursor'));
-  rfb.set_shared(UI.getSetting('shared'));
-  rfb.set_view_only(UI.getSetting('view_only'));
-  rfb.set_repeaterID(UI.getSetting('repeaterID')); */
-
-  // rfb.set_encrypt(false);
-  // rfb.set_true_color(true);
-  // rfb.set_local_cursor(true);
-  // rfb.set_shared(true);
-  // rfb.set_view_only(false);
-  // rfb.set_repeaterID(true);
-
-  // rfb.connect('localhost', 8000, 'password', '/');
+  if (secure) {
+    rfb.set_encrypt(true);
+  }
   rfb.connect(host, port, undefined, path);
 
   const keyboard = new Keyboard({
@@ -65,4 +40,7 @@ const _connect = ({canvas, host, port, path, ondisconnect}) => {
   };
 };
 
-window.connect = _connect;
+const novnc = {
+  connect: _connect,
+};
+module.exports = novnc;
